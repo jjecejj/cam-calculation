8718e25f2b8d741654abca5218b0c186745ac95b
MAA <you@example.com>
2025-12-06 01:33:24 +0300

    added graphs from time and console checking of limit parameters

--- Polidain/drafts/main.py
+++ Polidain/drafts/main.py
@@ -46,12 +46,6 @@ Cq = -2 * p * s * r / ((q - 2) * (r - q) * (q - p) * (s - q))
 Cr = 2 * p * q * s / ((r - 2) * (s - r) * (r - p) * (r - q))
 Cs = -2 * p * q * r / ((s - 2) * (s - p) * (s - q) * (s - r))
 
-# Ограничение по параметрам
-V_lim = 7 # Скорость [м/с]
-A_lim = 24500 # Ускорение [м/с^2]
-D_lim = 6 * 1e8 # Рывок [м/с^3]
-K_lim = 2 * 1e11 #Кракен [м/с^4]
-
 def C_fun(p):
     q = p + p - 2
     r = q + p - 2
@@ -154,23 +148,55 @@ C_list_4, k_list_4  = C_fun(1 + round(k_4))
 N = 1000
 X_1 = np.linspace(0, phi_1, N)
 Y_1 = []
+V_1 = []
+A_1 = []
+D_1 = []
+K_1 = []
 for i in X_1:
     Y_1.append(-h_phi(i, C_list_1, k_list_1, phi_1, fi_0=0, h_kn_max = z) + r0)
+    V_1.append(-v_phi(i, C_list_1, k_list_1, phi_1, fi_0=0, h_kn_max = z) + r0)
+    A_1.append(-a_phi(i, C_list_1, k_list_1, phi_1, fi_0=0, h_kn_max = z) + r0)
+    D_1.append(-d_phi(i, C_list_1, k_list_1, phi_1, fi_0=0, h_kn_max = z) + r0)
+    K_1.append(-k_phi(i, C_list_1, k_list_1, phi_1, fi_0=0, h_kn_max = z) + r0)
 
 X_2 = np.linspace(phi_1, phi_2, N)
 Y_2 = []
+V_2 = []
+A_2 = []
+D_2 = []
+K_2 = []
 for i in X_2:
     Y_2.append(-h_phi(i, C_list_2, k_list_2, phi_2, fi_0=phi_1, h_kn_max=h) + r0 + h)
+    V_2.append(-v_phi(i, C_list_2, k_list_2, phi_2, fi_0=phi_1, h_kn_max=h) + r0 + h)
+    A_2.append(-a_phi(i, C_list_2, k_list_2, phi_2, fi_0=phi_1, h_kn_max=h) + r0 + h)
+    D_2.append(-d_phi(i, C_list_2, k_list_2, phi_2, fi_0=phi_1, h_kn_max=h) + r0 + h)
+    K_2.append(-k_phi(i, C_list_2, k_list_2, phi_2, fi_0=phi_1, h_kn_max=h) + r0 + h)
 
 X_3 = np.linspace(phi_3, phi_4, N)
 Y_3 = []
+V_3 = []
+A_3 = []
+D_3 = []
+K_3 = []
 for i in X_3:
     Y_3.append(-h_phi(phi_4 - i + phi_3, C_list_3, k_list_3, phi_4, fi_0=phi_3, h_kn_max = h) + r0 + h)
+    V_3.append(v_phi(phi_4 - i + phi_3, C_list_3, k_list_3, phi_4, fi_0=phi_3, h_kn_max = h) + r0 + h)
+    A_3.append(-a_phi(phi_4 - i + phi_3, C_list_3, k_list_3, phi_4, fi_0=phi_3, h_kn_max = h) + r0 + h)
+    D_3.append(-d_phi(phi_4 - i + phi_3, C_list_3, k_list_3, phi_4, fi_0=phi_3, h_kn_max = h) + r0 + h)
+    K_3.append(-k_phi(phi_4 - i + phi_3, C_list_3, k_list_3, phi_4, fi_0=phi_3, h_kn_max = h) + r0 + h)
 
 X_4 = np.linspace(phi_4, phi_5, N)
 Y_4 = []
+V_4 = []
+A_4 = []
+D_4 = []
+K_4 = []
 for i in X_4:
     Y_4.append(-h_phi(phi_5 - i + phi_4, C_list_4, k_list_4, phi_5, fi_0=phi_4, h_kn_max = z) + r0)
+    V_4.append(v_phi(phi_5 - i + phi_4, C_list_4, k_list_4, phi_5, fi_0=phi_4, h_kn_max = z) + r0)
+    A_4.append(-a_phi(phi_5 - i + phi_4, C_list_4, k_list_4, phi_5, fi_0=phi_4, h_kn_max = z) + r0)
+    D_4.append(-d_phi(phi_5 - i + phi_4, C_list_4, k_list_4, phi_5, fi_0=phi_4, h_kn_max = z) + r0)
+    K_4.append(-k_phi(phi_5 - i + phi_4, C_list_4, k_list_4, phi_5, fi_0=phi_4, h_kn_max = z) + r0)
 
 t_1 = X_1 * (T / 360)
 t_2 = X_2 * (T / 360)
@@ -178,7 +204,7 @@ t_3 = X_3 * (T / 360)
 t_4 = X_4 * (T / 360)
 
 # Зависимости от грудуса поворота кулачка
-V_1_grad = np.gradient(Y_1, X_1[1] - X_1[0])
+'''V_1_grad = np.gradient(Y_1, X_1[1] - X_1[0])
 V_2_grad = np.gradient(Y_2, X_2[1] - X_2[0])
 V_3_grad = np.gradient(Y_3, X_3[1] - X_3[0])
 V_4_grad = np.gradient(Y_4, X_4[1] - X_4[0])
@@ -217,55 +243,112 @@ D_4_sek = np.gradient(A_4_sek, t_4[1] - t_4[0])
 K_1_sek = np.gradient(D_1_sek, t_1[1] - t_1[0])
 K_2_sek = np.gradient(D_2_sek, t_2[1] - t_2[0])
 K_3_sek = np.gradient(D_3_sek, t_3[1] - t_3[0])
-K_4_sek = np.gradient(D_4_sek, t_4[1] - t_4[0])
+K_4_sek = np.gradient(D_4_sek, t_4[1] - t_4[0])'''
 
 import matplotlib.pyplot as plt
 
 fig, axs = plt.subplots(5, 1, figsize=(8, 20))  # 5 строк, 1 столбец
-
+'''
 # --- 1. Координата ---
-t = np.concatenate((t_1, t_2, t_3, t_4))
-Y = np.concatenate((Y_1, Y_2, Y_3, Y_4))
-V = np.concatenate((V_1_sek, V_2_sek, V_3_sek, V_4_sek))
-A = np.concatenate((A_1_sek, A_2_sek, A_3_sek, A_4_sek))
-D = np.concatenate((D_1_sek, D_2_sek, D_3_sek, D_4_sek))
-K = np.concatenate((K_1_sek, K_2_sek, K_3_sek, K_4_sek))
-
-axs[0].plot(t, Y)
-axs[0].scatter(t[Y.argmax()], Y.max(), color='r')
+axs[0].plot(t_1, Y_1)
+axs[0].plot(t_2, Y_2)
+axs[0].plot([t_2[-1], t_3[0]], [r0 + h, r0 + h])
+axs[0].plot(t_3, Y_3)
+axs[0].plot(t_4, Y_4)
 axs[0].set_xlabel('t, c')
 axs[0].set_ylabel('координата толкателя, м')
-
 axs[0].grid(True)
 
 # --- 2. Скорость ---
-axs[1].plot(t, V)
-axs[1].scatter(t[V.argmax()], V.max(), color='r')
-axs[1].scatter(t[V.argmin()], V.min(), color='b')
+axs[1].plot(t_1, V_1_sek)
+axs[1].plot(t_2, V_2_sek)
+axs[1].plot([t_2[-1], t_3[0]], [0, 0])
+axs[1].plot(t_3, V_3_sek)
+axs[1].plot(t_4, V_4_sek)
 axs[1].set_xlabel('t, c')
 axs[1].set_ylabel('Скорость')
 axs[1].grid(True)
 
 # --- 3. Ускорение ---
-axs[2].plot(t, A)
-axs[2].scatter(t[A.argmax()], A.max(), color='r')
-axs[2].scatter(t[A.argmin()], A.min(), color='b')
+axs[2].plot(t_1, A_1_sek)
+axs[2].plot(t_2, A_2_sek)
+axs[2].plot([t_2[-1], t_3[0]], [0, 0])
+axs[2].plot(t_3, A_3_sek)
+axs[2].plot(t_4, A_4_sek)
 axs[2].set_xlabel('t, c')
 axs[2].set_ylabel('Ускорение')
 axs[2].grid(True)
 
 # --- 4. Рывок ---
-axs[3].plot(t, D)
-axs[3].scatter(t[D.argmax()], D.max(), color='r')
-axs[3].scatter(t[D.argmin()], D.min(), color='b')
+axs[3].plot(t_1, D_1_sek)
+axs[3].plot(t_2, D_2_sek)
+axs[3].plot([t_2[-1], t_3[0]], [0, 0])
+axs[3].plot(t_3, D_3_sek)
+axs[3].plot(t_4, D_4_sek)
 axs[3].set_xlabel('t, c')
 axs[3].set_ylabel('Рывок')
 axs[3].grid(True)
 
 # --- 5. "Кракен" ---
-axs[4].plot(t, K)
-axs[4].scatter(t[K.argmax()], K.max(), color='r')
-axs[4].scatter(t[K.argmin()], K.min(), color='b')
+axs[4].plot(t_1, K_1_sek)
+axs[4].plot(t_2, K_2_sek)
+axs[4].plot([t_2[-1], t_3[0]], [0, 0])
+axs[4].plot(t_3, K_3_sek)
+axs[4].plot(t_4, K_4_sek)
+axs[4].set_xlabel('t, c')
+axs[4].set_ylabel('Кракен')
+axs[4].grid(True)
+
+plt.tight_layout()
+plt.show()
+'''
+
+# --- 1. Координата ---
+axs[0].plot(t_1, Y_1)
+axs[0].plot(t_2, Y_2)
+axs[0].plot([t_2[-1], t_3[0]], [r0 + h, r0 + h])
+axs[0].plot(t_3, Y_3)
+axs[0].plot(t_4, Y_4)
+axs[0].set_xlabel('t, c')
+axs[0].set_ylabel('координата толкателя, м')
+axs[0].grid(True)
+
+# --- 2. Скорость ---
+axs[1].plot(t_1, V_1)
+axs[1].plot(t_2, V_2)
+axs[1].plot([t_2[-1], t_3[0]], [0, 0])
+axs[1].plot(t_3, V_3)
+axs[1].plot(t_4, V_4)
+axs[1].set_xlabel('t, c')
+axs[1].set_ylabel('Скорость')
+axs[1].grid(True)
+
+# --- 3. Ускорение ---
+axs[2].plot(t_1, A_1)
+axs[2].plot(t_2, A_2)
+axs[2].plot([t_2[-1], t_3[0]], [0, 0])
+axs[2].plot(t_3, A_3)
+axs[2].plot(t_4, A_4)
+axs[2].set_xlabel('t, c')
+axs[2].set_ylabel('Ускорение')
+axs[2].grid(True)
+
+# --- 4. Рывок ---
+axs[3].plot(t_1, D_1)
+axs[3].plot(t_2, D_2)
+axs[3].plot([t_2[-1], t_3[0]], [0, 0])
+axs[3].plot(t_3, D_3)
+axs[3].plot(t_4, D_4)
+axs[3].set_xlabel('t, c')
+axs[3].set_ylabel('Рывок')
+axs[3].grid(True)
+
+# --- 5. "Кракен" ---
+axs[4].plot(t_1, K_1)
+axs[4].plot(t_2, K_2)
+axs[4].plot([t_2[-1], t_3[0]], [0, 0])
+axs[4].plot(t_3, K_3)
+axs[4].plot(t_4, K_4)
 axs[4].set_xlabel('t, c')
 axs[4].set_ylabel('Кракен')
 axs[4].grid(True)
@@ -301,34 +384,3 @@ plt.ylabel('Y, м')
 plt.grid(True)
 plt.title("Профиль кулачка")
 plt.show()
-
-def out_red(text):
-    print("\033[34m{}".format(text))
-
-V_max = V.max()
-V_min = V.min()
-print('Максимальная скорость толкателя:', V_max, ' м/с')
-print('Минимальная скорость толкателя:', V_min, ' м/с')
-if abs(V_max) > V_lim or abs(V_min) > V_lim:
-    out_red('Превышен придел скорости')
-print('\n')
-A_max = A.max()
-A_min = A.min()
-print('Максимальное ускорение толкателя:', A_max, ' м/с^2')
-print('Минимальное ускорение толкателя:', A_min, ' м/с^2')
-if abs(A_max) > A_lim or abs(A_min) > A_lim:
-    out_red('Превышен придел ускорения')
-print('\n')
-D_max = D.max()
-D_min = D.min()
-print('Максимальный рывок толкателя:', D_max, ' м/с^3')
-print('Минимальный рывок толкателя:', D_min, ' м/с^3')
-if abs(D_max) > D_lim or abs(D_min) > D_lim:
-    out_red('Превышен придел рывка')
-print('\n')
-K_max = K.max()
-K_min = K.min()
-print('Максимальный кракен толкателя:', K_max, ' м/с^4')
-print('Минимальный кракен толкателя:', K_min, ' м/с^4')
-if abs(K_max) > K_lim or abs(K_min) > K_lim:
-    out_red('Превышен придел кракена')
--- Polidain/test.py
+++ Polidain/test.py
@@ -1,4 +1,56 @@
-import math
-x = -0.5
-y = 0.5
-print(math.atan2(x, y))
+import numpy as np
+a = np.array([1, 2, 3])
+
+'''
+axs[0].plot(t_1, Y_1)
+axs[0].plot(t_2, Y_2)
+axs[0].plot([t_2[-1], t_3[0]], [r0 + h, r0 + h])
+axs[0].plot(t_3, Y_3)
+axs[0].plot(t_4, Y_4)
+axs[0].set_xlabel('t, c')
+axs[0].set_ylabel('координата толкателя, м')
+axs[0].grid(True)
+
+# --- 2. Скорость ---
+axs[1].plot(t_1, V_1_sek)
+axs[1].plot(t_2, V_2_sek)
+axs[1].plot([t_2[-1], t_3[0]], [0, 0])
+axs[1].plot(t_3, V_3_sek)
+axs[1].plot(t_4, V_4_sek)
+axs[1].set_xlabel('t, c')
+axs[1].set_ylabel('Скорость')
+axs[1].grid(True)
+
+# --- 3. Ускорение ---
+axs[2].plot(t_1, A_1_sek)
+axs[2].plot(t_2, A_2_sek)
+axs[2].plot([t_2[-1], t_3[0]], [0, 0])
+axs[2].plot(t_3, A_3_sek)
+axs[2].plot(t_4, A_4_sek)
+axs[2].set_xlabel('t, c')
+axs[2].set_ylabel('Ускорение')
+axs[2].grid(True)
+
+# --- 4. Рывок ---
+axs[3].plot(t_1, D_1_sek)
+axs[3].plot(t_2, D_2_sek)
+axs[3].plot([t_2[-1], t_3[0]], [0, 0])
+axs[3].plot(t_3, D_3_sek)
+axs[3].plot(t_4, D_4_sek)
+axs[3].set_xlabel('t, c')
+axs[3].set_ylabel('Рывок')
+axs[3].grid(True)
+
+# --- 5. "Кракен" ---
+axs[4].plot(t_1, K_1_sek)
+axs[4].plot(t_2, K_2_sek)
+axs[4].plot([t_2[-1], t_3[0]], [0, 0])
+axs[4].plot(t_3, K_3_sek)
+axs[4].plot(t_4, K_4_sek)
+axs[4].set_xlabel('t, c')
+axs[4].set_ylabel('Кракен')
+axs[4].grid(True)
+
+plt.tight_layout()
+plt.show()
+'''
